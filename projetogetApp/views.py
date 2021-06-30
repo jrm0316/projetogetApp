3746394e27ef76b40e0a2d39980aa7a6d727876c
Juliano Madeira <julianorodrigues0316@gmail.com>
2021-06-30 09:02:09 -0300

    Initial commit

---
+++ projetogetApp/views.py
@@ -0,0 +1,38 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def hello(request):
+    return render(request, 'index.html')
+
+def articles(request, year):
+    return HttpResponse('O ano passado foi: ' +str(year))
+
+def lerDoBanco(nome):
+    lista_nomes = [
+        {'nome': 'Ana', 'idade': 20},
+        {'nome': 'Pedro', 'idade': 25},
+        {'nome': 'Joaquim', 'idade': 27}
+     ]
+
+    for pessoa in lista_nomes:
+        if pessoa['nome'] == nome:
+            return pessoa
+        else:
+            return {'nome': 'Nome não encontrado', 'idade': 0}
+
+
+def fname(request, nome):
+    result = lerDoBanco(nome)
+    if result ['idade'] > 0:
+        return HttpResponse('A pessoa foi encontrada, ela tem ' + str(result['idade']) + ' anos')
+    else:
+        return HttpResponse('Pessoa nao encontrada')
+
+
+def fname2(request, nome):
+    idade = lerDoBanco(nome)['idade']
+    return render(request,'pessoa.html', {'v_idade': idade})
+
+def persons_list(request):
+    return render(request, 'pessoa.html')
